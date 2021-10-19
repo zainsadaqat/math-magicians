9b02f4e921aaad97040aa49e3614c4f915dbe75d
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-10-19 20:40:04 +0500

    Create Calculator.js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Calculator</h1>;
+  }
+}
+
+export default Calculator;
