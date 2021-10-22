362bff3e57f2936d0f01f306635e9ef5490c2fc6
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-10-22 23:39:20 +0500

    Convert class-based button component into function component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { value, clickEvent, className } = this.props;
-    return (
-      <button
-        type="button"
-        className={className}
-        value={value}
-        onClick={clickEvent}
-      >
-        {value}
-      </button>
-    );
-  }
-}
+const Button = (props) => {
+  const { value, clickEvent, className } = props;
+  return (
+    <button
+      type="button"
+      className={className}
+      value={value}
+      onClick={clickEvent}
+    >
+      {value}
+    </button>
+  );
+};
 
 Button.propTypes = {
   value: PropTypes.string.isRequired,
