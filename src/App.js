06189afd8cfc8b6ece5b64ffe7feec4bf9c01406
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-10-22 23:40:29 +0500

    Convert class-based App component into function component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
 
 export default App;
